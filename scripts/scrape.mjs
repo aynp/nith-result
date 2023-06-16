bc61c4401ae6eca645a087d788ecbc6210eba3f7
Aryan <75756768+aynp@users.noreply.github.com>
2023-06-17 02:53:51 +0530

    Speed up writing to database

--- scripts/scrape.mjs
+++ scripts/scrape.mjs
@@ -8,7 +8,7 @@ const prisma = new PrismaClient();
 let parseErrorCount = 0;
 let fetchErrCount = 0;
 
-async function parseHTML(rawHTML, batch, rollNo) {
+async function parseHTML(rawHTML, batch) {
     try {
         const dom = new JSDOM(rawHTML);
 
@@ -18,7 +18,7 @@ async function parseHTML(rawHTML, batch, rollNo) {
         // table[1] contains information about the student
         const studentInfo = tables[1].querySelectorAll('td');
 
-        // const rollNo = studentInfo[0].querySelectorAll('p')[1].textContent.trim().toLowerCase();
+        const rollNo = studentInfo[0].querySelectorAll('p')[1].textContent.trim().toLowerCase();
 
         //* Will only work with with roll numbers such as 20bcs020
         const branch_code = rollNo.substring(2, 5);
@@ -26,83 +26,21 @@ async function parseHTML(rawHTML, batch, rollNo) {
         const name = studentInfo[1].querySelectorAll('p')[1].textContent.trim();
         const fathersName = studentInfo[2].querySelectorAll('p')[1].textContent.trim();
 
-        const student = await prisma.student.upsert({
-            create: {
-                name,
-                rollno: rollNo,
-                fathers_name: fathersName,
-                batch: batch,
-                branch_code: branch_code,
-            },
-            update: {},
-            where: {
-                rollno: rollNo
-            }
-        })
+        const student = {
+            name,
+            rollno: rollNo,
+            fathers_name: fathersName,
+            batch: batch,
+            branch_code: branch_code,
+        }
 
-        // console.log(student);
+        const courses = [];
+        const results = [];
+        const semSummaries = [];
 
         // table[2] through table[n-3] contains the result of the student in a pair of two!
         for (let i = 2; i < tables.length - 2; i += 2) {
             let semNo = i / 2;
-
-            //* sem_summary needs to be created first as it is a foreign key in results
-            // table[i+1] contains the summary of the student in a particular semester
-            const semSummary = tables[i + 1].querySelectorAll('td');
-
-            const sgpaEq = semSummary[1].querySelectorAll('p')[1].textContent.trim();
-            const sgpi = sgpaEq.substring(sgpaEq.indexOf('=') + 1).trim();
-
-            const sgpiTotal = parseInt(semSummary[2].querySelectorAll('p')[1].textContent.trim());
-
-            const cgpiEq = semSummary[3].querySelectorAll('p')[1].textContent.trim();
-            const cgpi = cgpiEq.substring(cgpiEq.indexOf('=') + 1).trim();
-
-            const cgpiTotal = parseInt(semSummary[4].querySelectorAll('p')[1].textContent.trim());
-
-            await prisma.sem_summary.upsert({
-                create: {
-                    rollno: rollNo,
-                    semester: semNo,
-                    sgpi: sgpi,
-                    sgpi_total: sgpiTotal,
-                    cgpi: cgpi,
-                    cgpi_total: cgpiTotal
-                },
-                update: {
-                    sgpi: sgpi,
-                    sgpi_total: sgpiTotal,
-                    cgpi: cgpi,
-                    cgpi_total: cgpiTotal
-                },
-                where: {
-                    rollno_semester: {
-                        rollno: rollNo,
-                        semester: semNo
-                    }
-                }
-            });
-
-            await prisma.summary.upsert({
-                create: {
-                    rollno: rollNo,
-                    semester: semNo,
-                    sgpi: sgpi,
-                    sgpi_total: sgpiTotal,
-                    cgpi: cgpi,
-                    cgpi_total: cgpiTotal
-                },
-                update: {
-                    semester: semNo,
-                    sgpi: sgpi,
-                    sgpi_total: sgpiTotal,
-                    cgpi: cgpi,
-                    cgpi_total: cgpiTotal
-                },
-                where: {
-                    rollno: rollNo
-                }
-            });
 
             // table[i] contains the detailed result of the student in a particular semester
             const semInfo = tables[i].querySelectorAll('tr');
@@ -118,53 +56,94 @@ async function parseHTML(rawHTML, batch, rollNo) {
                 const gradeLetter = subjectInfo[4].textContent.trim();
                 const GP = parseInt(subjectInfo[5].textContent.trim());
 
-                await prisma.course.upsert({
-                    create: {
-                        code: courseCode,
-                        title: courseName,
-                        credits: courseCredits
-                    },
-                    update: {},
-                    where: {
-                        code: courseCode
-                    }
+                courses.push({
+                    code: courseCode,
+                    title: courseName,
+                    credits: courseCredits
                 })
 
-                await prisma.result.upsert({
-                    create: {
-                        rollno: rollNo,
-                        grade: GP / courseCredits,
-                        code: courseCode,
-                        semester: semNo,
-                        grade_letter: gradeLetter,
-                        gp: GP
-                    },
-                    update: {
-                        grade: GP / courseCredits,
-                        grade_letter: gradeLetter,
-                        gp: GP
-                    },
-                    where: {
-                        rollno_code_semester: {
-                            code: courseCode,
-                            rollno: rollNo,
-                            semester: semNo
-                        }
-                    }
+                results.push({
+                    rollno: rollNo,
+                    code: courseCode,
+                    semester: semNo,
+                    grade: GP / courseCredits,
+                    grade_letter: gradeLetter,
+                    gp: GP
                 })
             }
 
+            // table[i+1] contains the summary of the student in a particular semester
+            const semSummary = tables[i + 1].querySelectorAll('td');
+
+            const sgpaEq = semSummary[1].querySelectorAll('p')[1].textContent.trim();
+            const sgpi = sgpaEq.substring(sgpaEq.indexOf('=') + 1).trim();
+
+            const sgpiTotal = parseInt(semSummary[2].querySelectorAll('p')[1].textContent.trim());
+
+            const cgpiEq = semSummary[3].querySelectorAll('p')[1].textContent.trim();
+            const cgpi = cgpiEq.substring(cgpiEq.indexOf('=') + 1).trim();
+
+            const cgpiTotal = parseInt(semSummary[4].querySelectorAll('p')[1].textContent.trim());
+
+            semSummaries.push({
+                // rollno: rollNo,
+                semester: semNo,
+                sgpi: sgpi,
+                sgpi_total: sgpiTotal,
+                cgpi: cgpi,
+                cgpi_total: cgpiTotal
+            });
         }
+
+        // Insert all the data into the database
+        await prisma.student.upsert({
+            create: {
+                ...student,
+                branch_code: branch_code,
+                sem_summary: {
+                    createMany: {
+                        data: semSummaries,
+                        skipDuplicates: true
+                    }
+                },
+                summary: {
+                    create: semSummaries[semSummaries.length - 1]
+                }
+            },
+            update: {
+                ...student,
+                sem_summary: {
+                    createMany: {
+                        data: semSummaries,
+                        skipDuplicates: true
+                    }
+                },
+                summary: {
+                    update: semSummaries[semSummaries.length - 1],
+                }
+            },
+            where: {
+                rollno: rollNo
+            }
+        });
+
+        await prisma.course.createMany({
+            data: courses,
+            skipDuplicates: true
+        });
+
+        await prisma.result.createMany({
+            data: results,
+            skipDuplicates: true
+        });
     } catch (err) {
+        console.log(err);
         parseErrorCount++;
     }
 }
 
-async function fetchBatch(location, batch) {
-    const emailsAll = fs.readFileSync(`./${location}/${batch}`, 'utf-8');
-
-    // remove all error files if present
-    fs.existsSync(`./error/${batch}`) && fs.unlinkSync(`./error/${batch}`);
+async function fetchBatch(batch) {
+    const emailsAll = fs.readFileSync(`./data/${batch}`, 'utf-8');
 
     const emails = emailsAll.split(',');
 
@@ -177,9 +156,8 @@ async function fetchBatch(location, batch) {
                 timeout: 5000
             });
 
-            await parseHTML(res.data, batch, rollNo);
+            await parseHTML(res.data, batch);
         } catch (err) {
-            fs.appendFileSync(`./error/${batch}`, email + ',');
             console.log(err);
             fetchErrCount++;
         }
@@ -191,18 +169,7 @@ async function main() {
 
     for (const batch of files) {
         console.log(`Processing ${batch} batch`);
-        await fetchBatch('data', batch);
-    };
-
-    console.log("Failed to parse for", parseErrorCount, "students."); parseErrorCount = 0;
-    console.log("Failed to fetch for", fetchErrCount, "students."); fetchErrCount = 0;
-
-    // try again for roll numbers in error files
-    const errorFiles = fs.readdirSync('./error');
-
-    for (const batch of errorFiles) {
-        console.log(`Processing ${batch} batch`);
-        await fetchBatch('error', batch);
+        await fetchBatch(batch);
     };
 
     console.log("Failed to parse for", parseErrorCount, "students.");
